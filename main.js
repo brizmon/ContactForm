d7e5bd569ecf525afbb4411bbb8381b17b4657b5
Briseida Montiel <brizism@gmail.com>
2017-10-23 19:47:05 -0400

    Initialize firebase

---
+++ main.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyANWy3zSJiIg_B1GTq_5Fy3lrkjYszZuIo",
+    authDomain: "form-b07bd.firebaseapp.com",
+    databaseURL: "https://form-b07bd.firebaseio.com",
+    projectId: "form-b07bd",
+    storageBucket: "form-b07bd.appspot.com",
+    messagingSenderId: "238200157645"
+  };
+  firebase.initializeApp(config);
+
+
+
